4ae2115a04f99b0f10b7dd6a085eca67d9cfe9cc
CHEAV Sovannarith <cheavsovannarith@gmail.com>
2021-09-07 16:26:56 +0700

    Initial commit

---
+++ src/main/kotlin/sovannarith/info/githubactivities/GithubActivitiesApplication.kt
@@ -0,0 +1,11 @@
+package sovannarith.info.githubactivities
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActivitiesApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubActivitiesApplication>(*args)
+}
